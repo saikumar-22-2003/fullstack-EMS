5fa5af7e872ae9e4456501e729498794ba6078c2
saikumar-22-2003 <jennesaikumar589@gmail.com>
2026-04-19 10:34:01 +0530

    First commit

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  
+  
+  
+)
+//   git config --global user.email "you@example.com"
+// git config --global user.name "Your Name"
+
